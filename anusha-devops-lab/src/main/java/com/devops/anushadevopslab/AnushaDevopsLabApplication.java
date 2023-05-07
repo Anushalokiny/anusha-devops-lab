82f06fa165bf9ce8c9e0b615726a768a13351ac1
Anushalokiny <anushalokiny@gmail.com>
2023-05-07 20:54:25 +0530

    first commit

---
+++ anusha-devops-lab/src/main/java/com/devops/anushadevopslab/AnushaDevopsLabApplication.java
@@ -0,0 +1,13 @@
+package com.devops.anushadevopslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnushaDevopsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnushaDevopsLabApplication.class, args);
+	}
+
+}
